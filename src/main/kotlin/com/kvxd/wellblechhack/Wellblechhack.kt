0548a99ae1bddd34b7c5d52856f64105a4b7af3b
meo209 <redslyice@outlook.de>
2025-01-17 20:02:15 +0100

    initial

---
+++ src/main/kotlin/com/kvxd/wellblechhack/Wellblechhack.kt
@@ -0,0 +1,11 @@
+package com.kvxd.wellblechhack
+
+import net.fabricmc.api.ClientModInitializer
+
+class Wellblechhack : ClientModInitializer {
+
+    override fun onInitializeClient() {
+
+    }
+
+}
